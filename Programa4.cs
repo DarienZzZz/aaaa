5e072d52711036913f60195876499fa4efbe06bb
DarienZzZz <146400531+DarienZzZz@users.noreply.github.com>
2023-10-01 21:23:11 -0600

    Add files via upload

---
+++ Programa4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Grados
+{
+    static void Main(string[] args)
+    {
+        int cen, fare;
+
+        Console.WriteLine("Ingrese los grados celcius:");
+        cen = Int32.Parse(Console.ReadLine());
+
+        fare= (cen * 9/5) + 32
+
+        Console.WriteLine("El perimetro es: " + fare);
+    }
+}
